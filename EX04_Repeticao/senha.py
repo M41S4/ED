438bb5177a31462a8bc8efa626c268e0630893b5
MaísaCB <97261344+M41S4@users.noreply.github.com>
2025-10-03 22:04:44 -0300

    Código senha incompleta

---
+++ EX04_Repeticao/senha.py
@@ -0,0 +1,13 @@
+## 4) Programa - senha: Defina uma senha correta dentro do programa 
+## (por exemplo: "1234"). Depois, peça para o usuário digitar uma senha. 
+##Enquanto a senha digitada for diferente da senha correta, imprimir "❌Senha incorreta!" ,  q
+##uando a senha correta for digitada,  imprimir"✅Senha correta!".
+##Dica: Use uma repetição (while) e um operador relacional de comparação == (igual) ou != (diferente).
+
+
+senha_correta = "1234"
+senha_user = input()
+
+while senha_user != senha_correta:
+  print("Senha incorreta")
+    if 
