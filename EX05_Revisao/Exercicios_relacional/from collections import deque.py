27fb5054c4dca0c5c9496284345cf32b5c4f5d40
MaísaCB <97261344+M41S4@users.noreply.github.com>
2025-10-31 22:01:22 -0300

    Exercício fila

---
+++ EX05_Revisao/Exercicios_relacional/from collections import deque.py
@@ -0,0 +1,23 @@
+from collections import deque 
+fila = deque()
+
+opcaoDesejada = 0
+    
+while opcaoDesejada != 4:
+
+    opcaoDesejada = (int(input(
+    "---MENU---\n"
+    "1-Adicionar pessoa à fila\n2-Atender pessoa\n3-Mostrar fila\n4-Encerrar atendimento\n"
+    "Escolha a opção desejada:"
+    )))
+        
+    if opcaoDesejada == 1:
+        nomePaciente = str(input("Digite o nome do paciente: "))
+        fila.append(nomePaciente)
+        print(f"fila{fila}")
+    elif opcaoDesejada == 2:
+        primeiroRemovido = fila.popleft()
+        print(f"paciente atendido: {primeiroRemovido}")  
+    elif opcaoDesejada == 3:
+        print(fila)
+        
